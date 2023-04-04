c3fcd26b4d8edd567db9a107e249e51811f0c1af
Coyocon <leroz_@hotmail.com>
2023-04-04 17:05:17 -0600

    inde

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php 
-    require_once("Controllers/loginContoller.php");
+/*     require_once("Controllers/loginContoller.php");
     
-    $loginController = new loginController; 
+    $loginController = new loginController;
+
     $metodo = $_SERVER["REQUEST_METHOD"];
     if($metodo == "GET"){
         $loginController->login();
     }
+ */
+
+ require_once "Connect/Conexion.php";
+
+  $respuesta = Conexion::conectar()->prepare("SELECT * FROM usuarios ");
+  $respuesta->execute();
+  $users = $respuesta->fetchAll(PDO::FETCH_ASSOC);
+ var_dump($users);
 ?>

7472139da7197d1fa2ec95da63b06ca10ab1bac4
Coyocon <leroz_@hotmail.com>
2023-04-04 16:48:46 -0600

    hola

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 /*     require_once("Controllers/loginContoller.php");
     
     $loginController = new loginController; 
-
     $metodo = $_SERVER["REQUEST_METHOD"];
-
     if($metodo == "GET"){
         $loginController->login();
     }

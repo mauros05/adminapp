c37b1ea338900d608cdc52d75e91e6c7e6567a36
Coyocon <leroz_@hotmail.com>
2023-04-05 17:01:50 -0600

    index actulizacion

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php 
 /*     require_once("Controllers/loginContoller.php");
     
-    $loginController = new loginController;
-
+    $loginController = new loginController; 
     $metodo = $_SERVER["REQUEST_METHOD"];
     if($metodo == "GET"){
         $loginController->login();
     }
  */
- require_once "Connect/Conexion.php";
 
+/*  require_once "Connect/Conexion.php";
   $respuesta = Conexion::conectar()->prepare("SELECT * FROM usuarios ");
   $respuesta->execute();
   $users = $respuesta->fetchAll(PDO::FETCH_ASSOC);
- var_dump($users);
-?>
+ var_dump($users); */
+
+/* Requiere Controladores */
+require_once "Controllers/plantillaController.php";
+
+
+
+$plantilla = new PlantillaController();
+$plantilla->ctrPlantilla();

d896e282d2ee47de1af32799cfdd4eb8bb73b973
Nancy Perez Lopez <nancy.perezl@softtekprojects.com>
2023-04-05 18:19:24 -0600

    se agregó Reportescontroller y view(resumenFinanciero)

---
+++ Views/resumenFinanciero.php
@@ -0,0 +1,60 @@
+<div class="content-wrapper">
+    <section class="content-header">
+        <div class="container-fluid">
+
+        <div class="login-box">
+        <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+            <a href="" class="h1"><b>Resumen </b>Financiero</a>
+            </div>
+            <div class="card-body">
+            <p class="login-box-msg">Ingreso: $</p>
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">MES</h3>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+
+                <div class="box-body">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="chart-responsive">
+                                <canvas id="pieChart" height="150"></canvas>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <ul class="chart-legend clearfix">
+                                <li><i class="fa fa-circle-o text-red"></i>Gastos</li>
+                                <li><i class="fa fa-circle-o text-green"></i>Inversiones </li>
+                                <li><i class="fa fa-circle-o text-yellow"></i>Ahorros</li>
+                                <li><i class="fa fa-circle-o text-aqua"></i>Créditos</li>
+                            
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer no-padding">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li><a href="#">United States of America
+                            <span class="pull-right text-red"><i class="fa fa-angle-down"></i> 12%</span></a>
+                        </li>
+                        <li><a href="#">India <span class="pull-right text-green"><i class="fa fa-angle-up"></i> 4%</span></a>
+                        </li>
+                        <li><a href="#">China
+                        <span class="pull-right text-yellow"><i class="fa fa-angle-left"></i> 0%</span></a></li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+        </div>
+    </div>
+ </section>
+</div>
